be892a5027953bd1fefd50f3dc1181f16229bdab
John Fabricatore <94406047+jayfabs@users.noreply.github.com>
2023-04-12 19:02:50 -0400

    commit test

--- app.py
+++ app.py
@@ -1,49 +1,61 @@
-import datetime as dt
-import numpy as np
-from sqlalchemy import *
+from sqlalchemy import create_engine, Table, MetaData, func
+from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from sqlalchemy import Table, MetaData
 from flask import Flask, jsonify, request
-from sqlalchemy import create_engine, MetaData
 
-
-engine = create_engine("sqlite:///Resources/sport_caps.db")
+engine = create_engine("sqlite:///Resources/airbnb_data.db")
 metadata = MetaData()
 metadata.reflect(bind=engine)
 Base = automap_base()
-Base.prepare(engine)
+Base.prepare(engine, reflect=True)
 
-data = Base.classes.airbnb_data
-Players = Base.classes.Players
+airbnb = Base.classes.airbnb_clean
 
-session = Session(engine)
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-from flask import request
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-    
-@app.route('/shutdown', methods=['POST'])
-def shutdown():
-    shutdown_server()
-    return 'Server shutting down...'
+@app.teardown_request
+def remove_session(*args):
+    Session.remove()
+
 @app.route("/")
 def welcome():
-    return(
-        f"Welcome To The Sport Cap API<br/>"
-        f"We Currently Offer Cap/Salary #s for Football <br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/teams<br/>"
-        f"/api/v1.0/players<br/>"
-        f"/api/v1.0/teams/name<br/>"
-        f"/api/v1.0/players/name<br/>"
-        f"<p> /team/'name' should be full name of desired team with '_' in spaces. For example new york giants =  New_York_Giants"
-        f"<p> /players/'name' should be full name of player with spaces for example 'Saquon Barkley' "
+    return (
+        "Welcome To The Mealbourne Airbnb Data API<br/>"
+        "We Currently Offer Data On Over 20k Airbnbs In Melbourne <br/>"
+        "Available Routes:<br/>"
+        "/api/v1.0/GeoJson<br/>"
+        "/api/v1.0/players<br/>"
+        "/api/v1.0/teams/name<br/>"
+        "/api/v1.0/players/name<br/>"
+        "<p> /team/'name' should be full name of desired team with '_' in spaces. For example new york giants =  New_York_Giants"
+        "<p> /players/'name' should be full name of player with spaces for example 'Saquon Barkley' "
     )
+
+@app.route("/api/v1.0/GeoJson")
+def all():
+    features = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    for row in Session.query(airbnb).all():
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [row.Longitude, row.Latitude]
+            },
+            "properties": {
+                "description": row.Description,
+                "host_name": row.Host_Name,
+                "index": row.index
+            }
+        }
+        features["features"].append(feature)
+    return jsonify(features)
+
+if __name__ == "__main__":
+    app.run(debug=True)
