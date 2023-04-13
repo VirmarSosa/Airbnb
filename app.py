18aab79bebea013450125cf52faf09dfd1afeb37
Your Name <sonila15@gmail.com>
2023-04-13 15:48:32 -0500

    added new files

--- app.py
+++ app.py
@@ -110,18 +110,18 @@ def available():
     return jsonify(result)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+# if __name__ == "__main__":
+#     app.run(debug=True)
+# from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///airbnb_data.db'
-db = SQLAlchemy(app)
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///airbnb_data.db'
+# db = SQLAlchemy(app)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
